1deeba68054972387310be2913bd1abc6bed7f3b
jhoa.mar18 <jhoa.mar18@alumnes.ilerna.com>
2025-09-23 13:10:09 +0200

    Laboratorio 1 - Ejercicio 2 completado: Uso de variables y operaciones

---
+++ laboratorio1/ejercicio2/script.js
@@ -0,0 +1,22 @@
+// declaramos las variables
+let numero = 10;
+let cadena = "Hola";
+let booleano = true;
+
+// probamos operaciones matemáticas
+let suma = numero + 5;
+let resta = numero - 2;
+let multiplicacion = numero * 3;
+let division = numero / 2;
+let modulo = numero % 3;
+
+// mostramos resultados en la consola
+console.log("Suma:", suma);
+console.log("Resta:", resta);
+console.log("Multiplicación:", multiplicacion);
+console.log("División:", division);
+console.log("Módulo:", modulo);
+
+// concatenamos una cadena con un número
+let concatenacion = cadena + " " + numero;
+console.log("Concatenación:", concatenacion);
